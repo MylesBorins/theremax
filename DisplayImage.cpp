d1ef8676936aba0d64da7b7453ecac5c56df7797
Myles Borins <myles.borins@gmail.com>
2013-11-30 05:54:08 -0800

    Basic depth through brightness

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,3 +1,4 @@
+//http://docs.opencv.org/modules/imgproc/doc/histograms.html?highlight=histogram#CvHistogram*%20cvCreateHist%28int%20dims,%20int*%20sizes,%20int%20type,%20float**%20ranges,%20int%20uniform%29
 #include <iostream>
 #include <highgui.h>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -6,6 +7,26 @@ using namespace std;
 using namespace cv;
 
 #define CAMERA_OUTPUT_WINDOW_NAME "camera-output"
+
+void _getBrightness(const Mat& frame, double& brightness)
+{
+    Mat temp, color[3], lum;
+    temp = frame;
+
+    split(temp, color);
+
+    color[0] = color[0] * 0.299;
+    color[1] = color[1] * 0.587;
+    color[2] = color[2] * 0.114;
+
+
+    lum = color[0] + color [1] + color[2];
+
+    Scalar summ = sum(lum);
+
+
+    brightness = summ[0]/((::pow(2,8)-1)*frame.rows * frame.cols) * 2; //-- percentage conversion factor
+ }
 
 int main(int argc, char **argv)
 {
@@ -22,9 +43,17 @@ int main(int argc, char **argv)
 
     while (true) {
         Mat cameraFrame;
+        
         camStream >> cameraFrame;
         if (cameraFrame.dims != 0)
-            imshow(CAMERA_OUTPUT_WINDOW_NAME, cameraFrame);
+        {
+            Mat frameHSV;
+            double brightness;
+            _getBrightness(cameraFrame, brightness);
+            cerr << brightness << endl;
+            imshow( CAMERA_OUTPUT_WINDOW_NAME, cameraFrame );
+        }
+            
         if (waitKey(30) == 27) {
             cout << "Input" << endl;
             break;
@@ -32,7 +61,7 @@ int main(int argc, char **argv)
     }
 
     cout << "Done" << endl;
-
+    camStream.release();
     cvDestroyWindow(CAMERA_OUTPUT_WINDOW_NAME);
     return 1;
 }
