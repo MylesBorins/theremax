7191d2ea169bee764ae1e335bbf4cbd435e642e1
Myles Borins <myles.borins@gmail.com>
2013-12-12 04:43:49 -0800

    Initial cabin commit

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pages: {
+      posts: {
+        src: 'posts',
+        dest: 'dist',
+        layout: 'src/layouts/post.jade',
+        url: 'posts/:title/',
+        options: {
+          pageSrc: 'src/pages',
+          data: {
+            baseUrl: '/'
+          },
+          pagination: {
+            postsPerPage: 1,
+            listPage: 'src/pages/index.jade'
+          }
+        }
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          sassDir: 'src/styles',
+          cssDir: 'dist/styles'
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          dest: 'dist',
+          src: [
+            'images/**',
+            'scripts/**',
+            'styles/**.css',
+            'styles/fonts/**',
+          ]
+        }]
+      }
+    },
+    watch: {
+      pages: {
+        files: [
+          'posts/**',
+          'src/layouts/**',
+          'src/pages/**'
+        ],
+        tasks: ['pages']
+      },
+      compass: {
+        files: ['src/styles/**'],
+        tasks: ['compass']
+      },
+      copy: {
+        files: [
+          'src/images/**',
+          'src/scripts/**',
+          'src/styles/**.css',
+          'src/styles/fonts/**'
+        ],
+        tasks: ['copy']
+      },
+      dist: {
+        files: ['dist/**'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      dist: {
+        options: {
+          port: 5455,
+          hostname: '0.0.0.0',
+          base: 'dist',
+          livereload: true
+        }
+      }
+    },
+    open: {
+      dist: {
+        path: 'http://localhost:5455'
+      }
+    },
+    clean: {
+      dist: 'dist'
+    },
+  });
+
+  grunt.registerTask('build', [
+    'clean',
+    'pages',
+    'compass',
+    'copy'
+  ]);
+
+  
+
+  grunt.registerTask('server', [
+    'build',
+    'connect',
+    'open',
+    'watch'
+  ]);
+
+  grunt.registerTask('default', 'server');
+
+  require('load-grunt-tasks')(grunt);
+};
